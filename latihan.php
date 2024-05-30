26f482a41f8dfe0a40063d6e2e39376380a8a88a
MesonWakerkwa <168641598+MesonWakerkwa@users.noreply.github.com>
2024-05-30 21:49:58 +0700

    Add files via upload

---
+++ latihan.php
@@ -0,0 +1,15 @@
+<?php 
+$sisi=10;
+$sisi=10; 
+$teks1="Belajar Menghitung volume"; 
+$teks2="persegi panjang"; 
+$volume = $sisi*$sisi*$sisi;
+echo"$teks1 . $teks2<br>"; 
+echo"Panjang Sisi Kubus = $sisi<br>"; 
+echo"sisi lebar persegi panjang = $sisi<br>";
+echo"menghitung persegi panjang <br>";
+echo"rumus panjang x lebar<br>";
+echo "pangjang =10 cm<br>";
+echo "lebar    =10 cm<br>";
+echo"Volume Kubus = $volume  cm2<br>"; 
+?>
---
+++ luas_persegi_panjang.php
@@ -0,0 +1,18 @@
+<?php
+  echo "Kode Program PHP - Keliling dan Luas Persegi Panjang";
+  echo "<hr>";
+ 
+  $panjang = 15;
+  $lebar = 30;
+ 
+  echo "Panjang persegi = $panjang <br>";
+  echo "Lebar persegi = $lebar <br>";
+ 
+  echo "<br>";
+ 
+  echo "Keliling persegi panjang = (2 * $panjang) + (2 * $lebar) 
+        = ".(2 * $panjang) + (2 * $lebar)." <br>";
+ 
+  echo "Luas persegi panjang = $panjang * $lebar
+        = ".$panjang * $lebar." <br>";
+?>
